22ed23091c5b228864c1a72c32c0f144ae7bb317
omar-ashraf2 <oashraf304@gmail.com>
2023-11-02 23:17:39 +0200

    fix

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./page.module.css";
-import Button from "@/components/Button/Button";
 import Image from "next/image";
 import { items } from "./data.js";
 import { notFound } from "next/navigation";
+import Button from "@/components/button/Button";
 
 const getData = (cat) => {
   const data = items[cat];
